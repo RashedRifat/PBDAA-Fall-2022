0d961a0863c85afaaa6c9aa1bb283af2efb1ed8f
Rashed Rifat <rmr557@nyu.edu>
2022-11-14 13:24:05 -0500

    adding join table commands

--- ghg_emissions/hive/make_table.sh
+++ ghg_emissions/hive/make_table.sh
@@ -8,3 +8,15 @@ CREATE EXTERNAL TABLE ghg (iso STRING, source STRING, year  BIGINT, co2 DOUBLE) 
 # Create Tree Data Table 
 CREATE EXTERNAL TABLE tree (iso STRING, year  BIGINT, treeLoss DOUBLE, co2 DOUBLE) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE LOCATION '<path>';
 ALTER TABLE tree SET TBLPROPERTIES ("skip.header.line.count"="1");
+
+# Join Table 
+CREATE TABLE combined as 
+SELECT tree.iso as iso, tree.year as year, tree.treeloss as treeloss, ghg.source as sector, ghg.co2 as co2 
+FROM tree JOIN ghg 
+ON (tree.iso = upper(ghg.iso) and tree.year = ghg.year); 
+
+# Check that the table worked 
+SELECT * FROM combined LIMIT 10;
+
+# Count Rows 
+SELECT COUNT(*) FROM combined; 
